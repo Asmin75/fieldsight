e2ea76f74eb4129c04fb7acc43aa766ff58e5dfc
aasis <aashish.baidya.c3@gmail.com>
2018-09-17 13:46:57 +0545

    fixed bug in report xls generator

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -652,7 +652,7 @@ def exportProjectSiteResponses(task_prog_obj_id, source_user, project_id, base_u
             row=[]
 
             for col_num in range(len(head_columns)):
-                row.append(head_columns[col_num]['question_label'], "")    
+                row.append(head_columns[col_num].get('question_label', ""))    
                 ws[0]=row
 
             
