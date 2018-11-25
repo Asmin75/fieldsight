01dfb081cbf2733c6a5c43786d4fe817c9d2794c
aasis <aashish.baidya.c3@gmail.com>
2018-11-25 14:31:04 +0545

    fixed tasks import issues

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -151,7 +151,7 @@ def generate_stage_status_report(task_prog_obj_id, project_id):
         site_objs = Site.objects.filter(project_id=137)
         print "Checkpoint 1 started"
         for site_obj in site_objs:
-            site_dict[site_obj.id] = {'visits':0,'site_status':site_obj.site_status, 'latitude':site_obj.latitude,'longitude':site_obj.longitude}
+            site_dict[site_obj.id] = {'visits':"No",'site_status':site_obj.site_status, 'latitude':site_obj.latitude,'longitude':site_obj.longitude}
         print "Checkpoint 1 finished"
         for site_visit in site_visits:
             try:
@@ -160,9 +160,10 @@ def generate_stage_status_report(task_prog_obj_id, project_id):
                 pass
         print "Checkpoint 2 finished"
         for site in sites:
-            site_row = [site['identifier'], site['name'], site['region__identifier'], site['address'], site_dict[site.get('id')]['latitude'], site_dict[site.get('id')]['longitude'], site_dict[site.get('id')]['site_status']]
-            print site
             import pdb; pdb.set_trace();
+            
+            site_row = [site['identifier'], site['name'], site['region__identifier'], site['address'], site_dict[site.get('id')]['latitude'], site_dict[site.get('id')]['longitude'], site_dict[site.get('id')]['site_status']]
+            
             for stage in ss_index:
                 site_row.append(site.get(stage, ""))
 
