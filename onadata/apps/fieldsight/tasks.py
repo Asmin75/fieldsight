654f1de3ef1c10129bc9a9a93f09b065665d7ac0
aasis <aashish.baidya.c3@gmail.com>
2018-11-25 13:53:15 +0545

    fixed tasks import issues

--- onadata/apps/fieldsight/tasks.py
+++ onadata/apps/fieldsight/tasks.py
@@ -151,14 +151,17 @@ def generate_stage_status_report(task_prog_obj_id, project_id):
         site_objs = Site.objects.filter(project_id=137)
         print "Checkpoint 1 started"
         for site_obj in site_objs:
-            site_dict[site_obj.id] = {'visits':'','site_status':site_obj.site_status, 'latitude':site_obj.latitude,'longitude':site_obj.longitude}
+            site_dict[site_obj.id] = {'visits':0,'site_status':site_obj.site_status, 'latitude':site_obj.latitude,'longitude':site_obj.longitude}
         print "Checkpoint 1 finished"
         for site_visit in site_visits:
-            site_dict[site_visit['_id']]['visits'] = len(site_visit['visits'])
+            try:
+                site_dict[site_visit['_id']]['visits'] = len(site_visit['visits'])
+            except:
+                pass
         print "Checkpoint 2 finished"
         for site in sites:
             site_row = [site['identifier'], site['name'], site['region__identifier'], site['address'], site_dict[site.get('id')]['latitude'], site_dict[site.get('id')]['longitude'], site_dict[site.get('id')]['site_status']]
-            
+
             for stage in ss_index:
                 site_row.append(site.get(stage, ""))
 
