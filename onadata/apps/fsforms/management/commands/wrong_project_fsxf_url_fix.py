e0a3329887806858b10d59a1bec1eb7c985e8cc1
awemulya <awemulya@gmail.com>
2018-12-18 16:10:42 +0545

    clean faulty project form

--- onadata/apps/fsforms/management/commands/wrong_project_fsxf_url_fix.py
+++ onadata/apps/fsforms/management/commands/wrong_project_fsxf_url_fix.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 
-from onadata.apps.fsforms.models import FInstance
+from onadata.apps.fsforms.models import FInstance, FieldSightXF
 
 
 class Command(BaseCommand):
@@ -13,6 +14,15 @@ class Command(BaseCommand):
         finstances = FInstance.objects.filter(project_fxf=sent_project_form_id, site__in=site_ids)
         for fi in finstances:
             if fi.project_fxf.xf.id_string != fi.instance.survey_type.slug:
+                original_form = FieldSightXF.objects.get(pk=original_project_form_id)
+                with transaction.atomic():
+                    fi.project_fxf = original_form
+                    fi.save()
+                    i = fi.instance
+                    i.xform = original_form.xf
+                    i.is_synced_with_mongo = False
+                    i.save()
+
                 self.stdout.write('Faulty   .. "%s"' % str(fi.instance.id))
             else:
                 self.stdout.write('ok   .. "%s"' % str(fi.instance.id))
